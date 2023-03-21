fc0a1953fe05defeeefe5ec29c26e0f0a1ed21ed
forster <jmforster2@gmail.com>
2023-03-20 21:59:38 -0300

    Implementado menu, listar projetos e requisitos e integracao com SQLite

--- app/src/main/java/com/example/projeto_integrador_ix/MainActivity.java
+++ app/src/main/java/com/example/projeto_integrador_ix/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.projeto_integrador_ix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,34 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button btnIrParaCriarProjeto = (Button)findViewById(R.id.btnIrParaCriarProjeto);
-        btnIrParaCriarProjeto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,CriarProjetoActivity.class);
-                startActivity(intent);
-            }
-        });
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater exibirMenu = getMenuInflater();
+        exibirMenu.inflate(R.menu.menu, menu);
+        return true;
+    }
 
-        Button btnIrParaCriarRequisito = (Button) findViewById(R.id.btnIrParaCriarRequisito);
-
-        btnIrParaCriarRequisito.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,CriarRequisitoActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button btnListarProjeto = findViewById(R.id.btnListarProjetos);
-
-        btnListarProjeto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ListarProjetoActivity.class);
-                startActivity(intent);
-            }
-        });
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.submenu_criar_projeto:
+                startActivity(new Intent(MainActivity.this,CriarProjetoActivity.class));
+                return true;
+            case R.id.submenu_listar_projetos:
+                startActivity(new Intent(MainActivity.this,ListarProjetoActivity.class));
+                return true;
+            case R.id.submenu_criar_requisito:
+                startActivity(new Intent(MainActivity.this,CriarRequisitoActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
